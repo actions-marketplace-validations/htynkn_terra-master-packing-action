3d71c32fad1a4b989caddf7f88bebba112ef2770
htynkn <htynkn@gmail.com>
2021-07-13 11:12:25 +0800

    enable output

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,14 @@ function print_info() {
     echo -e "\e[36mINFO: ${1}\e[m"
 }
 
-echo $(pwd)
+OUTPUT=$(pwd)
 
 FOLDER_NAME=$1
 ARCH=$2
 TPK_NAME=$3
 
 mkdir -p "/tool/$ARCH/$TPK_NAME"
-cp "$FOLDER_NAME" "/tool/$ARCH/$TPK_NAME"
+cp -r "$FOLDER_NAME" "/tool/$ARCH/$TPK_NAME"
 print_info "packing for folder \"$FOLDER_NAME\""
 print_info "use arch \"$ARCH\""
 
@@ -27,5 +27,7 @@ cd /tool
 print_info 'start packing'
 
 ./makeapp "$ARCH/$TPK_NAME"
+
+cp "dist/$ARCH/*.tp" "$OUTPUT"
 
 print_info 'packing success'
